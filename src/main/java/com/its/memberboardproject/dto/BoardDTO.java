90e6c3cf2525ac0d9d829a2f0720db6b81d760b9
jinseo08 <kjs_8086@naver.com>
2022-07-03 11:54:39 +0900

    board DTO

--- src/main/java/com/its/memberboardproject/dto/BoardDTO.java
+++ src/main/java/com/its/memberboardproject/dto/BoardDTO.java
@@ -1,4 +1,25 @@
 package com.its.memberboardproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BoardDTO {
+
+    private Long id;
+    private String boardTitle;
+    private String boardWriter;
+    private String boardContents;
+    private int boardHits;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+    private String boardFileName;
+    private MultipartFile boardFile;
+
 }
