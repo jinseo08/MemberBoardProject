6a467e044b571396598a2729f89eb5888c4795f1
jinseo08 <kjs_8086@naver.com>
2022-07-10 15:07:49 +0900

    글작성 컨트롤러

--- src/main/java/com/its/memberboardproject/controller/BoardController.java
+++ src/main/java/com/its/memberboardproject/controller/BoardController.java
@@ -1,7 +1,28 @@
 package com.its.memberboardproject.controller;
 
+import com.its.memberboardproject.dto.BoardDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.io.IOException;
 
 @Controller
 public class BoardController {
+
+
+
+    //글 작성 화면 이동
+    @GetMapping("/save-form")
+    public String saveForm() {
+        return "/boardPages/saveForm";
+    }
+    //글 작성 처리
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) throws IOException {
+        boardService.save(boardDTO);
+        return "redirect:/board";
+    }
+
 }
