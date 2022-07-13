90dbd8bc8655881f03ef0df8f410d34f184a123d
jinseo08 <kjs_8086@naver.com>
2022-07-13 16:32:02 +0900

    BoardController 수정2

--- src/main/java/com/its/memberboardproject/controller/BoardController.java
+++ src/main/java/com/its/memberboardproject/controller/BoardController.java
@@ -1,17 +1,23 @@
 package com.its.memberboardproject.controller;
 
 import com.its.memberboardproject.dto.BoardDTO;
+import com.its.memberboardproject.service.BoardService;
+import com.its.memberboardproject.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 
 @Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
 
-
+    private final BoardService boardService;
 
     //글 작성 화면 이동
     @GetMapping("/save-form")
