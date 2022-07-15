4df689681df89a2f365c04d50026f0baaa505a44
jinseo08 <kjs_8086@naver.com>
2022-07-16 04:41:10 +0900

    BoardController 페이징, 디테일

--- src/main/java/com/its/memberboardproject/controller/BoardController.java
+++ src/main/java/com/its/memberboardproject/controller/BoardController.java
@@ -31,4 +31,26 @@ public class BoardController {
         return "redirect:/board";
     }
 
+    //페이징 전체 조회
+    @GetMapping
+    public String paging(@PageableDefault(page = 1) Pageable pageable, Model model) {
+        Page<BoardDTO> boardList = boardService.paging(pageable);
+        model.addAttribute("boardList", boardList);
+        int startPage = (((int) (Math.ceil((double) pageable.getPageNumber() / PagingConst.BLOCK_LIMIT))) - 1) * PagingConst.BLOCK_LIMIT + 1;
+        int endPage = ((startPage + PagingConst.BLOCK_LIMIT - 1) < boardList.getTotalPages()) ? startPage + PagingConst.BLOCK_LIMIT - 1 : boardList.getTotalPages();
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        return "boardPages/paging";
+    }
+    //디테일 상세조회
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        List<CommentDTO> commentDTOList = commentService.findAll(id);
+        model.addAttribute("commentList", commentDTOList);
+        return "boardPages/detail";
+    }
+
+
 }
