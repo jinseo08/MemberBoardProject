2caa3358097b0480208ddad1a41efd6f23b36627
jinseo08 <kjs_8086@naver.com>
2022-07-25 21:32:23 +0900

    BoardDTO 추가

--- src/main/java/com/its/memberboardproject/dto/BoardDTO.java
+++ src/main/java/com/its/memberboardproject/dto/BoardDTO.java
@@ -22,4 +22,38 @@ public class BoardDTO {
     private String boardFileName;
     private MultipartFile boardFile;
 
+
+    public BoardDTO(String boardTitle, String boardWriter, String boardContents, int boardHits, String boardFileName) {
+        this.boardTitle = boardTitle;
+        this.boardWriter = boardWriter;
+        this.boardContents = boardContents;
+        this.boardHits = boardHits;
+        this.boardFileName = boardFileName;
+    }
+
+    public BoardDTO(Long id, String boardTitle, String boardWriter, String boardContents, int boardHits, LocalDateTime createdDate, LocalDateTime updatedDate, String boardFileName) {
+        this.id = id;
+        this.boardTitle = boardTitle;
+        this.boardWriter = boardWriter;
+        this.boardContents = boardContents;
+        this.boardHits = boardHits;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.boardFileName = boardFileName;
+
+    }
+
+    public static BoardDTO toBoardDTO(BoardEntity boardEntity) {
+        BoardDTO boardDTO = new BoardDTO();
+        boardDTO.setId(boardEntity.getId());
+        boardDTO.setBoardTitle(boardEntity.getBoardTitle());
+        boardDTO.setBoardWriter(boardEntity.getBoardWriter());
+        boardDTO.setBoardContents(boardEntity.getBoardContents());
+        boardDTO.setBoardHits(boardEntity.getBoardHits());
+        boardDTO.setCreatedDate(boardEntity.getCreatedTime());
+        boardDTO.setUpdatedDate(boardEntity.getUpdatedTime());
+        boardDTO.setBoardFileName(boardEntity.getBoardFileName());
+        return boardDTO;
+    }
+
 }
