a1efe101889ba552612614be3fe47e19dceec2f6
jinseo08 <kjs_8086@naver.com>
2022-07-31 01:16:50 +0900

    CommentController 추가

--- src/main/java/com/its/memberboardproject/controller/CommentController.java
+++ src/main/java/com/its/memberboardproject/controller/CommentController.java
@@ -1,4 +1,29 @@
 package com.its.memberboardproject.controller;
 
+import com.its.memberboardproject.dto.CommentDTO;
+import com.its.memberboardproject.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/comment")
 public class CommentController {
+    private final CommentService commentService;
+
+    //댓글 저장
+    @PostMapping("/save")
+    public String save(@ModelAttribute CommentDTO commentDTO) {
+        System.out.println("CommentController.save");
+        commentService.save(commentDTO);
+        return "redirect:/board/" + commentDTO.getBoardId();
+    }
+
+    //댓글 삭제
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id){
+        commentService.delete(id);
+        return "redirect:/board/";
+    }
 }
