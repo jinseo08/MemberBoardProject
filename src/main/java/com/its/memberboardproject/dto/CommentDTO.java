7ca9745b54f12c27a03ab0bb56c6337dd047cc62
jinseo08 <kjs_8086@naver.com>
2022-08-06 11:59:23 +0900

    CommentDTO 추가

--- src/main/java/com/its/memberboardproject/dto/CommentDTO.java
+++ src/main/java/com/its/memberboardproject/dto/CommentDTO.java
@@ -1,4 +1,19 @@
 package com.its.memberboardproject.dto;
 
 public class CommentDTO {
+    private Long id;
+    private Long boardId;
+    private String commentWriter;
+    private String commentContents;
+    private LocalDateTime commentCreatedTime;
+
+    public static CommentDTO toSaveDTO(CommentEntity commentEntity) {
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(commentEntity.getId());
+        commentDTO.setCommentWriter(commentEntity.getCommentWriter());
+        commentDTO.setCommentContents(commentEntity.getCommentContents());
+        commentDTO.setCommentCreatedTime(commentEntity.getCreatedTime());
+
+        return commentDTO;
+    }
 }
