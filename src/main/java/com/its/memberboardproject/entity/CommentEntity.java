03ccc41c56ce3cf03f186f3465b0214dc8493248
jinseo08 <kjs_8086@naver.com>
2022-08-07 14:25:08 +0900

    CommentEntity 추가

--- src/main/java/com/its/memberboardproject/entity/CommentEntity.java
+++ src/main/java/com/its/memberboardproject/entity/CommentEntity.java
@@ -1,4 +1,31 @@
 package com.its.memberboardproject.entity;
 
 public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String commentWriter;
+
+    @Column
+    private String commentContents;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private MemberEntity memberEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private BoardEntity boardEntity;
+
+    public static CommentEntity toSaveEntity(CommentDTO commentDTO, BoardEntity boardEntity, MemberEntity memberEntity) {
+        CommentEntity commentEntity = new CommentEntity();
+        commentEntity.setCommentWriter(memberEntity.getMemberEmail());
+        commentEntity.setCommentContents(commentDTO.getCommentContents());
+        commentEntity.setBoardEntity(boardEntity);
+        commentEntity.setMemberEntity(memberEntity);
+        return commentEntity;
+    }
+
 }
