30908e1f840485633bbfc175081b8b5787a2c5b5
jinseo08 <kjs_8086@naver.com>
2022-08-15 04:45:08 +0900

    BoardRepository 수정

--- src/main/java/com/its/memberboardproject/repository/BoardRepository.java
+++ src/main/java/com/its/memberboardproject/repository/BoardRepository.java
@@ -4,4 +4,10 @@ import com.its.memberboardproject.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<BoardEntity,Long> {
+
+    @Modifying
+    @Query(value = "update BoardEntity b set b.boardHits = b.boardHits+1 where b.id= :id")
+    void boardHits(@Param("id") Long id);
+
+    List<BoardEntity> findByBoardTitleContainingOrBoardContentsContaining(String q1, String q2);
 }
