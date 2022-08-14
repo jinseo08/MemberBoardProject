f4031a9d1c287638a5dba9be64cb3e7ea563d00a
jinseo08 <kjs_8086@naver.com>
2022-08-14 14:44:12 +0900

    HomeController 추가

--- src/main/java/com/its/memberboardproject/controller/HomeController.java
+++ src/main/java/com/its/memberboardproject/controller/HomeController.java
@@ -1,7 +1,14 @@
 package com.its.memberboardproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(){
+        return "/";
+    }
 }
