825c6bcb49765ae281da47e40349af61c09ac243
jinseo08 <kjs_8086@naver.com>
2022-08-20 01:46:24 +0900

    BoardService 수정

--- src/main/java/com/its/memberboardproject/service/BoardService.java
+++ src/main/java/com/its/memberboardproject/service/BoardService.java
@@ -6,12 +6,16 @@ import com.its.memberboardproject.entity.MemberEntity;
 import com.its.memberboardproject.repository.BoardRepository;
 import com.its.memberboardproject.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Service
