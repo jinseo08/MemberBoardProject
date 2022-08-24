f30397aa011cc5eeec4ebaca604952c853a56530
jinseo08 <kjs_8086@naver.com>
2022-08-24 09:33:43 +0900

    BoardService 수정

--- src/main/java/com/its/memberboardproject/service/BoardService.java
+++ src/main/java/com/its/memberboardproject/service/BoardService.java
@@ -15,6 +15,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
