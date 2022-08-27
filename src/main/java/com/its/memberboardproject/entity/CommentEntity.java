0829593c9428d9a2017922e518f9c3430b5c290c
jinseo08 <kjs_8086@naver.com>
2022-08-27 18:48:34 +0900

    CommentEntity 수정

--- src/main/java/com/its/memberboardproject/entity/CommentEntity.java
+++ src/main/java/com/its/memberboardproject/entity/CommentEntity.java
@@ -1,5 +1,10 @@
 package com.its.memberboardproject.entity;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class CommentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
